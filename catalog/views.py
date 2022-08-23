74d8fac8de0cfa0fd1bb3b89acb731196bebfa96
smileoops <dinisa2010@gmail.com>
2022-08-23 14:26:37 +0300

    urls+views+htmls

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all()
+    num_instance = BookInstance.objects.all().count()
+    num_instance_avi = BookInstance.objects.filter(status__exact='д').count()
+    num_authors = Author.objects.count()
+    genre = Genre.objects.filter().count()
+    book_count = Book.objects.count()
+    return render(request, 'index.html', context={
+        'num_books': num_books,
+        'num_instance': num_instance,
+        'num_instance_avi': num_instance_avi,
+        'num_authors': num_authors,
+        'genre': genre,
+        'book_count': book_count
+    })
